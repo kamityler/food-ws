0fbab9bae2614237c5ef48a78c439a261e753eb3
Danylo <bodnar.dp@gmail.com>
2023-02-08 14:31:34 +0200

    RegExAdded

--- js/script.js
+++ js/script.js
@@ -584,10 +584,10 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     nextButton.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == +width.replace(/\D/g, '') * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += +width.replace(/\D/g, '');
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -597,19 +597,15 @@ window.addEventListener("DOMContentLoaded", () => {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            curSlide.textContent = `0${slideIndex}`;
-        } else {
-            curSlide.textContent = slideIndex;
-        }
+        curSlideIndex(slideIndex);
         dots.forEach(dot => dot.style.opacity = '.5');
         dots[slideIndex - 1].style.opacity = 1;
     });
     prevButton.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = +width.replace(/\D/g, '') * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= +width.replace(/\D/g, '');
         }
         slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -619,11 +615,7 @@ window.addEventListener("DOMContentLoaded", () => {
             slideIndex--;
         }
 
-        if (slides.length < 10) {
-            curSlide.textContent = `0${slideIndex}`;
-        } else {
-            curSlide.textContent = slideIndex;
-        }
+        curSlideIndex(slideIndex);
         dots.forEach(dot => dot.style.opacity = '.5');
         dots[slideIndex - 1].style.opacity = 1;
     });
@@ -631,22 +623,24 @@ window.addEventListener("DOMContentLoaded", () => {
     dots.forEach(dot => {
         dot.addEventListener('click', (e) => {
             const slideTo = e.target.getAttribute('data-slide-to');
-            
+
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = +width.replace(/\D/g, '') * (slideTo - 1);
             slidesField.style.transform = `translateX(-${offset}px)`;
 
-            if (slides.length < 10) {
-                curSlide.textContent = `0${slideIndex}`;
-            } else {
-                curSlide.textContent = slideIndex;
-            }
-
+            curSlideIndex(slideIndex);
             dots.forEach(dot => dot.style.opacity = '.5');
             dots[slideIndex - 1].style.opacity = 1;
-
         });
     });
+
+    function curSlideIndex(index) {
+        if (slides.length < 10) {
+            curSlide.textContent = `0${index}`;
+        } else {
+            curSlide.textContent = index;
+        }
+    }
 
     //end   
 });
